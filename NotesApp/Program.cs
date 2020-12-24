94c323c30ed585997a366bd66ae12ba3f6872d31
Nguyen Trong Thanh <rambothanh1985@gmail.com>
2020-12-24 19:38:49 +0700

    init

--- NotesApp/Program.cs
+++ NotesApp/Program.cs
@@ -6,7 +6,32 @@ namespace NotesApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nameApp = @"
+ ██████╗ ██╗  ██╗██╗     ██████╗██╗  ██╗██╗   ██╗     █████╗ ██████╗ ██████╗ 
+██╔════╝ ██║  ██║██║    ██╔════╝██║  ██║██║   ██║    ██╔══██╗██╔══██╗██╔══██╗
+██║  ███╗███████║██║    ██║     ███████║██║   ██║    ███████║██████╔╝██████╔╝
+██║   ██║██╔══██║██║    ██║     ██╔══██║██║   ██║    ██╔══██║██╔═══╝ ██╔═══╝ 
+╚██████╔╝██║  ██║██║    ╚██████╗██║  ██║╚██████╔╝    ██║  ██║██║     ██║     
+ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═════╝╚═╝  ╚═╝ ╚═════╝     ╚═╝  ╚═╝╚═╝     ╚═╝     
+                                                                             
+";
+            Console.WriteLine(nameApp);
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Enter your Name...");
+            string userName = Console.ReadLine();
+            Console.WriteLine("1. Show all notes");
+            Console.WriteLine("2. Add a new note");
+            Console.WriteLine("3. Edit note");
+            Console.WriteLine("4. Delete note");
+            Console.Write("Your choice:");
+            string choice = Console.ReadLine();
+            Console.WriteLine("ban da chon: " + choice);
+
         }
+
+
+
     }
 }
